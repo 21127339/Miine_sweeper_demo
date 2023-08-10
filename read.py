fb6ab1f64744f8fda86dbb688cac92a767ea9ba1
Nguyen Cong Lap <lapnguyen120903@gmail.com>
2023-08-10 21:13:51 +0700

    Update read.py

--- read.py
+++ read.py
@@ -1,4 +1,7 @@
+from objects import *
+from func import *
 from init import *
+from method1 import *
 
 def ReadData(fileName):
     
@@ -17,11 +20,11 @@ def ReadData(fileName):
             if i != ' ' and i != '\n':
                 
                 if i == 'X':
-                    newCell = Cell(i, True)
+                    newCell = Cell(i, False, False)
                 elif i == '-':
-                    newCell = Cell(None, False)
+                    newCell = Cell(None, False, None)
                 else:
-                    newCell = Cell(i, True)
+                    newCell = Cell(int(i), True, True)
                 
                 cols += 1      
                 tempArray.append(newCell)
@@ -29,16 +32,80 @@ def ReadData(fileName):
         field.append(tempArray)
         temp = fi.readline()
     cols = cols // rows
-    PrintMinesField(field, rows, cols)
-    return field, rows, cols
+    PrintUnopenedMinesField(field, rows, cols)
+    return MineField(field, rows, cols)
+
+def SolveEachArea(clues, cellsAsVariable):
     
-def FindAllAvailableClues(field, rows, cols):
+    numberOfClues = len(clues)
+    numberOfCells = len(cellsAsVariable)
+    matrix = [[0 for _ in range(numberOfCells)] for _ in range(numberOfClues)]
+    
+    for i in range(numberOfClues):
         
-        for i in range(rows):
-            for j in range(cols):
-                
-                if field[i][j].value != 'X' and field[i][j].value != 0 and field[i][j].value != None:
-                    a = 0    
-   
+        print("Check matrix", i)
+        for j in clues[i].cells:
+            index = cellsAsVariable[j]
+            matrix[i][index] = 1
+            print(index, j)
+        
+        
+        matrix[i].append(clues[i].unexploredBomb) 
+    PrintMatrix(matrix)
+    print()
+    PrintMatrix(Gauss_elimination(matrix))
+    print()
+    Conclusion(Gauss_elimination(matrix), cellsAsVariable)
     
-ReadData("output.txt")
+def Solve(mineField):
+        
+    areas = mineField.SeperateField()
+    
+    for eachArea in areas:
+        
+        clues = []
+        count = -1
+        cellsAsVariable = dict()
+        
+        for i in eachArea:                
+            
+            x_i, y_i = i
+            unexploredBomb = mineField.field[x_i][y_i].value
+            adjCells = mineField.FindAllAdjacentCell(i)
+            temp = set()
+            
+            for j in adjCells:
+                
+                x_j, y_j = j
+                
+                if mineField.field[x_j][y_j].isSafe == None:
+                    temp.add(j)
+                    if j not in cellsAsVariable:
+                        count += 1
+                        cellsAsVariable.update({j : count})
+                        
+                elif mineField.field[x_j][y_j].isSafe == False:
+                    unexploredBomb -= 1
+            
+            if len(clues) == 0:
+                print("Clue: ", temp, unexploredBomb)   
+                clues.append(Clue(temp, len(temp), unexploredBomb))
+            else:
+                flag = True
+                
+                for j in clues:
+                    if len(temp) == 0 and unexploredBomb == 0:
+                        flag = False
+                    else:
+                        flag = j.IsDifferentClue(temp, unexploredBomb)
+                    
+                if flag:
+                    print("Clue: ", temp, unexploredBomb)        
+                    clues.append(Clue(temp, len(temp), unexploredBomb))
+        #clues = PriorityQueue(clues)
+        print()
+        print(cellsAsVariable)     
+        SolveEachArea(clues, cellsAsVariable)
+   
+mineField = ReadData("input.txt")
+Solve(mineField)
